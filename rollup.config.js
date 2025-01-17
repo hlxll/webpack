2f5ab7fd7fb01b90efa1e68b64680a798e255913
huanglin <9043198+hlxll0808@user.noreply.gitee.com>
2025-01-17 15:07:43 +0800

    rollup打包

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import livereload from 'rollup-plugin-livereload';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
+import svgr from '@svgr/rollup';
+import postcss from 'rollup-plugin-postcss';
+import less from 'rollup-plugin-less';
+
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/main.js',
+        format: 'cjs',
+        name: 'MyComponent',
+        sourcemap: true,
+        globals: {
+            jquery: '$'
+        }//使用external排除的文件，如果在本项目中使用的就需要给一个全局变量了，比如jquery的$
+    },
+    external: ['react', 'react-dom', 'antd', 'jquery'],
+    plugins: [
+        resolve(),
+        commonjs(),
+        babel({
+            exclude: 'node_modules/**',
+            presets: ['@babel/preset-react'],
+        }),
+        less({ // 处理 LESS
+            output: 'dist/main.css', // 输出你需要的 CSS 文件
+        }),
+        // postcss({
+        //     extract: true, // 提取样式到单独文件
+        //     minimize: true, // 压缩 CSS
+        //     sourceMap: true, // 允许生成 sourcemap
+        // }),
+        terser(),
+        livereload(),
+        image(),
+        svgr(),
+    ],
+};
